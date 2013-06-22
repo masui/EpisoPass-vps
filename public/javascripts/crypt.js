d861dc102af8d1007cd2755eaffc1ca5b96c06dc
Toshiyuki Masui <masui@pitecan.com>
2013-06-22 22:44:57 +0900

    MD5 function was not working for UTF8 string... modified crypt.js to convert a UTF string into a byte string before calculating MD5.

--- public/javascripts/crypt.js
+++ public/javascripts/crypt.js
@@ -23,9 +23,49 @@ function crypt_char(c,n){
     return chars[ind];
 }
 
+function utf2bytearray(text) {
+    var result = [];
+    if (text == null)
+        return result;
+    for (var i = 0; i < text.length; i++) {
+        var c = text.charCodeAt(i);
+        if (c <= 0x7f) {
+            result.push(c);
+        } else if (c <= 0x07ff) {
+            result.push(((c >> 6) & 0x1F) | 0xC0);
+            result.push((c & 0x3F) | 0x80);
+        } else {
+            result.push(((c >> 12) & 0x0F) | 0xE0);
+            result.push(((c >> 6) & 0x3F) | 0x80);
+            result.push((c & 0x3F) | 0x80);
+        }
+    }
+    return result;
+}
+
+function utf2bytestr(text) {
+    var result = "";
+    if (text == null)
+        return result;
+    for (var i = 0; i < text.length; i++) {
+        var c = text.charCodeAt(i);
+        if (c <= 0x7f) {
+            result += String.fromCharCode(c);
+        } else if (c <= 0x07ff) {
+            result += String.fromCharCode(((c >> 6) & 0x1F) | 0xC0);
+            result += String.fromCharCode((c & 0x3F) | 0x80);
+        } else {
+            result += String.fromCharCode(((c >> 12) & 0x0F) | 0xE0);
+            result += String.fromCharCode(((c >> 6) & 0x3F) | 0x80);
+            result += String.fromCharCode((c & 0x3F) | 0x80);
+        }
+    }
+    return result;
+}
+
 // crypt(crypt(s,data),data) == s になる
 function crypt(str,seeddata){
-    var hash = MD5_hexhash(seeddata);
+    var hash = MD5_hexhash(utf2bytestr(seeddata));
     var res = "";
     for(i=0;i<str.length;i++){
 	var j = i % 8;
@@ -35,3 +75,6 @@ function crypt(str,seeddata){
     }
     return res;
 }
+
+
+
