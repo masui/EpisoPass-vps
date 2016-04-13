f3b96ee0500a236ca6adefa69115f6f7d96d4fed
Toshiyuki Masui <masui@pitecan.com>
2016-04-13 15:02:23 +0900

    ...

--- lib/config.rb
+++ lib/config.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
-ROOT = "/home/masui/EpisoPass"
-FILEROOT = "/home/masui/EpisoPass/data"
+#ROOT = "/home/masui/EpisoPass"
+#FILEROOT = "/home/masui/EpisoPass/data"
+ROOT = "/Users/masui/EpisoPass"
+FILEROOT = "/Users/masui/EpisoPass/data"
 # FILEROOT = "/tmp"
