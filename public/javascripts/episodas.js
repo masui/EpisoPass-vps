ae0ab8f6c57e2b0f37e01ee4556c3d3a6c5174c8
Toshiyuki Masui <masui@pitecan.com>
2017-10-01 09:57:39 +0900

    removed unnecessary comments

--- public/javascripts/episodas.js
+++ public/javascripts/episodas.js
@@ -23,7 +23,7 @@ function display(){ // n番目の問題と答リストを設定
     }
 }
 
-function select(s){
+function select_answer(s){
     if(page < qas.length){
         selected.push(s);
     }
@@ -100,16 +100,14 @@ var buttondiv;
 var buttondivs = [];
 
 function mouseenter(div){
-    //console.log(`mouseenter ${div.attr('id')}`);
     curdiv = div
     if(mousedown){
         curdiv.css('background-color','#f3f3f3');
-        select(curdiv.attr('index'));
+        select_answer(curdiv.attr('index'));
     }
 }
 
 function mouseleave(div){
-    //console.log(`mouseleave ${div.attr('id')}`);
     curdiv = null;
     if(mousedown){
         if(finished){
@@ -128,9 +126,6 @@ function mouseleave(div){
 function mousemove(e){
     var mousex = (e.pageX ? e.pageX : e.originalEvent.touches[0].pageX);
     var mousey = (e.pageY ? e.pageY : e.originalEvent.touches[0].pageY);
-    if(curdiv){
-        //console.log(`mousemove: curdiv_id = ${curdiv.attr('id')}`);
-    }
     for(var i=0;i<answers.length;i++){
         //buttondiv = $(`#id${i}`);
         buttondiv = buttondivs[i];
@@ -140,56 +135,23 @@ function mousemove(e){
         buttonh = buttondiv.height();
         if(buttonx < mousex && buttonx+buttonw > mousex &&
            buttony < mousey && buttony+buttonh > mousey){
-            //console.log(`buttondiv = ${buttondiv.attr('id')}`);
-            //console.log(`curdiv = ${curdiv}`);
-            //if(curdiv) console.log(`curdiv = ${curdiv.attr('id')}`);
-            //if(curdiv == null || (curdiv.attr('id') != buttondiv.attr('id'))){
             if(curdiv != buttondiv){
                 if(curdiv){
-                    //console.log(`DIFF ${curdiv.attr('id')}, ${buttondiv.attr('id')}`);
-                }
-                else {
-                    //console.log(`DIFF null, ${buttondiv.attr('id')}`);
-                }
-                if(curdiv){
-                    //console.log("MOUSELEAVE");
                     mouseleave(curdiv);
                 }
-                //console.log("MOUSEENTER====================================");
                 mouseenter(buttondiv);
                 curdiv = buttondiv;
-                //console.log(`assign - buttondiv.attr('id') = ${buttondiv.attr('id')} curdiv.attr('id') = ${curdiv.attr('id')}`);
             }
             return;
         }
     }
     if(curdiv){
-        //console.log("LAST");
         mouseleave(curdiv);
         mousediv = null;
     }
 }
 
 function init(){
-
-    // 引数解析、JSON読み出し
-    //var name = location.search.substring(1);
-    //namehash = exports.MD5_hexhash(name);
-    //$.ajax({
-    //url: `./data/${namehash}`,
-    //dataType: 'json',
-    //async: false,
-    //success: function(json) {
-    //// alert(json.seed);
-    //data = json;
-    //}
-    //});
-    //
-    //seed = data['seed'];
-
-    //[name, seed] = getdata(location.search.substring(1));
-    //if(seed == null) seed = data['seed'];
-
     qas = data['qas'];
     page = 0;
     
@@ -213,8 +175,8 @@ function init(){
 
     // 問題領域
     var qdiv = $('<div>');
-    qdiv.text('...');
-    qdiv.height(100);
+    //qdiv.text('...');
+    qdiv.attr('height',100);
     qdiv.css('display','flex');
     qdiv.css('justify-content','center');
     qdiv.css('align-items','center');
@@ -233,117 +195,27 @@ function init(){
         div.attr('id',`id${i}`);
         center.append(div);
 
-        ////// div.hover(mouseenter,mouseleave);
-
-        //div.on('mouseenter',function(e){
-        //    mousediv = $(e.target);
-        //    if(mousedown){
-        //      mousediv.css('background-color','#f3f3f3');
-        //      select(mousediv.attr('index'));
-        //    }
-        //});
-        //div.on('mouseleave',function(e){
-        //    mousediv = null;
-        //    if(mousedown){
-        //      if(finished){
-        //          finish();
-        //      }
-        //      else {
-        //          if(selected.length > 0){
-        //              $(`#id${selected[selected.length-1]}`).css('background-color','#ccc');
-        //          }
-        //          page += 1;
-        //          display();
-        //      }
-        //    }
-        //});
-
-        //div.on('mousemove',function(e){
-        //    e.preventDefault();
-        //});
-        //div.on('touchmove',function(e){
-        //    e.preventDefault();
-        //});
-
         div.on('mousedown',function(e){
                 e.preventDefault();
                 mousedown = true;
                 curdiv = null;
                 mousemove(e);
-
-                // mousemove($(e.target));
-
-                //mouseenter($(e.target));
-                //curdiv │$(e.target);
-            
-            //if(curdiv){
-            //  curdiv.css('background-color','#f3f3f3');
-            //  select(mousediv.attr('index'));
-            //}
         });
         div.on('touchstart',function(e){
                 e.preventDefault();
                 mousedown = true;
                 curdiv = null;
                 mousemove(e);
-                /*
-            mousedown = true;
-            if($(e.target)){
-                mousediv = $(e.target);
-            }
-            if(mousediv){
-                mousediv.css('background-color','#f3f3f3');
-                select(mousediv.attr('index'));
-            }
-                */
         });
         div.on('mouseup',function(e){
-                /*
-                  if(finished){
-                  finish();
-                  }
-                  else {
-                  e.preventDefault();
-                  if(mousedown){
-                  if(selected.length > 0){
-                  $(`#id${selected[selected.length-1]}`).css('background-color','#ccc');
-                  }
-                  page += 1;
-                  display();
-                  }
-                  }
-                */
-            if(curdiv) mouseleave(curdiv);
-            mousedown = false;
-            curdiv = null;
-
-            //mousemove(e);
-
-            //mouseleave($(e.target));
-            //curdiv = null;
-            //mousemove(e);
-            //mouseleave(curdiv);
+                if(curdiv) mouseleave(curdiv);
+                mousedown = false;
+                curdiv = null;
         });
         div.on('touchend',function(e){
-            if(curdiv) mouseleave(curdiv);
-            mousedown = false;
-            curdiv = null;
-            /*
-            if(finished){
-                finish();
-            }
-            else {
-                e.preventDefault();
-                if(mousedown){
-                    if(selected.length > 0){
-                        $(`#id${selected[selected.length-1]}`).css('background-color','#ccc');
-                    }
-                    page += 1;
-                    display();
-                }
-            }
-            mousedown = false;
-            */
+                if(curdiv) mouseleave(curdiv);
+                mousedown = false;
+                curdiv = null;
         });
     }
 
